98f67b1c011651d78c52ef69b8378849471ef4b4
Scott Cote <scrcote@gmail.com>
2017-12-14 09:13:26 -0500

    re added 'listening' message

--- src/server/server.js
+++ src/server/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import http from 'http';
 
 const app = express();
+const port = process.env.PORT || 8080;
 
 app.use(express.static(__dirname));
 
@@ -9,4 +10,6 @@ app.get('*', (req, res) => {
   res.sendFile(`${__dirname}/index.html`);
 });
 
-http.createServer(app).listen(process.env.PORT || 8080);
+http.createServer(app).listen(port, () => {
+  console.log(`Listening for requests on port ${port}`);
+});
